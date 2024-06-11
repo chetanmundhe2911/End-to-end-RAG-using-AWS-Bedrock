c04ab59ea2c4dfd2067f8a7498aa3ca3332cecd7
chetanmundhe2911 <chetanmundhe2911@gmail.com>
2024-06-11 18:19:03 +0530

    code updated

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
-# End-to-end-RAG-using-AWS-Bedrock
+# End-to-end-RAG-using-Amazon-Bedrock
 
 
-##### Step1 -  Create  new enviroment
+## How to run?
 
+###  1.Create a new environment
+
+```bash
+conda create -n llmapp python=3.8 -y 
 ```
-conda create -n llmapp python =3.8 -y
+
+
+###  2.Activate the environment
+```bash
+conda activate llmapp 
 ```
 
 
-##### Step2 - install requirement file
 
-```
+###  3.Install the requirements package
+```bash
 pip install -r requirements.txt
+```
+
+
+###  4. run your application
+```bash
+streamlit run main.py
 ```
--- main.py
+++ main.py
@@ -9,39 +9,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 
 
-#Bedrock client
-bedrock = boto3.client(service_name = "bedrock-runtime", region_name = "us-east-1")
 
-#Get embeddings model from bedrock
-bedrock_embedding = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client= bedrock)
-
-
-def get_documents():
-    loader = PyPDFDirectoryLoader("data")
-    documents = loader.load()
-    text_spliter = RecursiveCharacterTextSplitter(
-                                        chunk_size=1000, 
-                                        chunk_overlap=500)
-    docs = text_spliter.split_documents(documents)
-    return docs
-
-
-#Store the vectors
-def get_vector_store(docs):
-   vectorstore_faiss =  FAISS.from_documents(
-        docs,
-        bedrock_embedding
-    )
-   vectorstore_faiss.save_local("faiss_local")
-
-
-
-#Call LLM model , here - mistral.mistral-7b-instruct-v0:2
-def get_llm():
-    llm = Bedrock(model_id = "mistral.mistral-7b-instruct-v0:2", client = bedrock)
-    return llm
-
-# Provide general prompt
 prompt_template = """
 
 Human: Use the following pieces of context to provide a 
@@ -57,15 +25,45 @@ Question: {question}
 Assistant:"""
 
 
+#Bedrock client
+bedrock = boto3.client(service_name = "bedrock-runtime", region_name = "us-east-1")
+
+#Get embeddings model from bedrock
+bedrock_embedding = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client= bedrock)
 
 
-## Pass the prompt to template
+
+def get_documents():
+    loader = PyPDFDirectoryLoader("Data")
+    documents = loader.load()
+    text_spliter = RecursiveCharacterTextSplitter(
+                                        chunk_size=1000, 
+                                        chunk_overlap=500)
+    docs = text_spliter.split_documents(documents)
+    return docs
+
+
+def get_vector_store(docs):
+   vectorstore_faiss =  FAISS.from_documents(
+        docs,
+        bedrock_embedding
+    )
+   vectorstore_faiss.save_local("faiss_local")
+
+
+
+def get_llm():
+    llm = Bedrock(model_id = "mistral.mistral-7b-instruct-v0:2", client = bedrock)
+    return llm
+
+
+
+
 PROMPT = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
 )
 
 
-# Pass the context & question to LLM to get the response
 def get_llm_response(llm, vectorstore_faiss, query):
 
     qa = RetrievalQA.from_chain_type(
@@ -80,3 +78,34 @@ def get_llm_response(llm, vectorstore_faiss, query):
     
     response = qa({"query": query})
     return response['result']
+
+
+
+def main():
+    st.set_page_config("RAG")
+    st.header("End to end RAG using Bedrock")
+
+    user_question = st.text_input("Ask a question from the PDF file")
+
+    with st.sidebar:
+        st.title("Update & create vectore store")
+
+        if st.button("Store Vector"):
+            with st.spinner("Processing.."):
+                docs = get_documents()
+                get_vector_store(docs)
+                st.success("Done")
+
+        if st.button("Send"):
+            with st.spinner("Processing.."):
+               faiss_index = FAISS.load_local("faiss_local", bedrock_embedding, allow_dangerous_deserialization=True) 
+               llm = get_llm()
+               st.write(get_llm_response(llm,faiss_index,  user_question))
+
+               
+
+
+
+
+if __name__ == "__main__":
+    main()
